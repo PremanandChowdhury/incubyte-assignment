2fe4c8077c4aa3a7330bd0c58cd663f5ce071edf
Premanand Chowdhury <freudigprem@gmail.com>
2024-12-24 17:19:59 +0530

    chore: code with new line between two adjacent numbers

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 export function sum(input) {
-  let result;
-  if (input === "") {
-    result = 0;
-  } 
+  console.log(input);
+  let result = 0;
+
+  if (input.trim() == "") {
+    return result;
+  }
+
+  let numbers = input.replace(/\n/g, ",").split(",");
+
+  // Calculate the sum of input numbers
+  if (numbers) {
+    result = numbers.reduce((acc, num) => acc + parseInt(num), 0);
+  }
 
   return result;
-};
+}
--- index.test.js
+++ index.test.js
@@ -1,5 +1,27 @@
-const sum = require("./index");
+import {sum } from "./index";
 
 test("returns 0 for an empty input string to sum ", () => {
     expect(sum("")).toBe(0);
+    expect(sum(" ")).toBe(0); // with space
+    expect(sum("  ")).toBe(0); // with multiple spaces
+});
+
+test("returns the sum of numbers with comma separated values", () => {
+    expect(sum("1,2,3")).toBe(6);
+});
+
+test("returns the sum of numbers with comma separated values along with spaces", () => {
+    expect(sum("1 ,2 , 3 , 4")).toBe(10);
+});
+
+test("returns the sum of numbers with comma separated with larger length", () => {
+    expect(sum("1 ,2 , 3, 4, 5, 6, 7, 8, 9, 10, 11")).toBe(66);
+});
+
+test("returns the sum of numbers with new line in between numbers", () => {
+    expect(sum("1\n2,3")).toBe(6);
+});
+
+test("returns the sum of numbers with multiple new line in between numbers", () => {
+    expect(sum("1\n2,3, 4\n4, 5, 6")).toBe(25);
 });
